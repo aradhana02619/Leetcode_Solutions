1d543efd086b97974dbdf368a76fc988a5cebd1b
aradhana02619 <aradhanachaurasia21@gmail.com>
2026-08-01 19:26:03 +0530

    Time: 235 ms (1.72%), Space: 29.7 MB (29.72%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n =nums.size();
+        k=k%n;
+        reverse(nums.begin(), nums.begin()+n-k);
+        reverse(nums.begin()+n-k, nums.end());
+
+        reverse(nums.begin(), nums.end());
+
+        for(int i=0; i<n; i++){
+            cout<<nums[i]<<",";
+        }
+        cout<<endl;
+        
+    }
+};
