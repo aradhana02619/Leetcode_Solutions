e3da0ca1b0c3843fb5db6b7999049b6c80a7f12c
aradhana02619 <aradhanachaurasia21@gmail.com>
2026-08-05 23:01:39 +0530

    Time: 144 ms (96.55%), Space: 297.7 MB (90%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<std::vector<int>> adj(n);
+        for (const auto& edge : invocations) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+
+        
+        vector<bool> is_suspicious(n, false);
+        queue<int> q;
+        
+        q.push(k);
+        is_suspicious[k] = true;
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+
+            for (int neighbor : adj[curr]) {
+                if (!is_suspicious[neighbor]) {
+                    is_suspicious[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        
+        for (const auto& edge : invocations) {
+            int u = edge[0];
+            int v = edge[1];
+            
+            if (!is_suspicious[u] && is_suspicious[v]) {
+                vector<int> all_methods(n);
+                iota(all_methods.begin(), all_methods.end(), 0);
+                return all_methods;
+            }
+        }
+
+    
+        std::vector<int> remaining;
+        for (int i = 0; i < n; ++i) {
+            if (!is_suspicious[i]) {
+                remaining.push_back(i);
+            }
+        }
+
+        return remaining;
+        
+    }
+};
