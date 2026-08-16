b7e84d58a43cfb2dcb84df58f04b4a78a5a6627a
aradhana02619 <aradhanachaurasia21@gmail.com>
2026-08-16 22:04:29 +0530

    Time: 7 ms (39.59%), Space: 131.5 MB (6.09%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+class Solution {
+public:
+    bool stoneGameIX(std::vector<int>& stones) {
+        
+        vector<int> count(3, 0);
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+    
+        if (count[0] % 2 == 0) {
+            return std::min(count[1], count[2]) > 0;
+        }
+    
+        return std::abs(count[1] - count[2]) > 2;
+    }
+};
